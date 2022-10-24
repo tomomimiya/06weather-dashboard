bea917af01cff1b1cdc63dd8bab357bea51301b6
tomomimiya <tomomimiya128@gmail.com>
2022-10-24 16:54:25 -0600

    modified html with bootstrap

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//AS A traveler
+//I WANT to see the weather outlook for multiple cities
+//SO THAT I can plan a trip accordingly
+
+//WHEN I search for a city
+//THEN I am presented with current and future conditions
+//for that city and that city is added to the search history
